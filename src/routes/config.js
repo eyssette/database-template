b85105c99adf0170ed820a1f219b75a7e1ec46a2
eyssette <cedric.eyssette@gmail.com>
2023-01-04 04:57:38 +0100

    Update config.js

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,8 +1,9 @@
 export let title = "Sujets de dissertation donnés au baccalauréat";
 
-export let src = "https://raw.githubusercontent.com/eyssette/sujets-philosophie/main/data/sujets-vrac.tsv";
+export let src = "https://raw.githubusercontent.com/eyssette/sujets-philosophie-bac/main/data/dissertations.tsv";
 /* https://raw.githubusercontent.com/eyssette/textes-philo/main/data/textes-new.tsv */
 /* https://raw.githubusercontent.com/eyssette/sujets-philosophie-bac/main/data/dissertations.tsv */
+/* https://raw.githubusercontent.com/eyssette/sujets-philosophie/main/data/sujets-vrac.tsv */
 
 export let contentBeforeTable = 'Site créé par <a href="http://eyssette.github.io/">Cédric Eyssette</a> à partir du travail de François Elie : <a href="https://philo-labo.fr/sujets/">https://philo-labo.fr/sujets/</a>'
 
@@ -23,7 +24,7 @@ export function groupColumns(arrInit) {
 }
 
 /* Pour changer le titre des colonnes  */
-export let dataNoHeader=true;
+export let dataNoHeader=false;
 export let changeHeader = false;
 export let newHeader=['Intitulé du sujet'];
 /* export let newHeader=['Auteur·e','Texte']; */
@@ -34,7 +35,8 @@ export let markText=true;
 /* Si les données sont déjà triées en ordre ascendant pour une colonne, il faut l'indiquer ici */
 export let historyColumnsClickDefault=[0]
 
-export let tableCSS='small'
+export let tableCSS=''
+/* export let tableCSS='small' */
 
 /* Pour indiquer des conditions supplémentaires possibles (cases à cocher) avec le format suivant : intitulé de la condition, numéro de la colonne concernée, regex correspondante */
 export let useAdditionalConditions = false;
