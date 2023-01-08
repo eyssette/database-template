a7c6920037ded4932425f7bb10a1a0030739c79b
eyssette <cedric.eyssette@gmail.com>
2023-01-08 13:58:12 +0100

    Updates

--- _app/immutable/chunks/1-620da652.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-113c04e0.js";export{t as component};
--- _app/immutable/chunks/2-18af308a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-ac3a72e6.js";export{t as component};
