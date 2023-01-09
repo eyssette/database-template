016c3ca9838c72dbd7aedb17b175c09d5c8177b9
eyssette <cedric.eyssette@gmail.com>
2023-01-09 06:00:04 +0100

    :bug: Tri des colonnes et :zap: Suppression des setTimeout non nécessaire
    Au lieu de faire seulement : $: if (textToSearch !== '') …
    il fallait avoir : $: if (textToSearch !== '' && previoustextToSearch !== textToSearch)
    
    Sinon, cela forçait la réactivation de rows notamment à cause du else à la fin, qui est lui aussi changé, en ajoutant une condition :  if (textToSearch == '')

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -34,100 +34,124 @@
 	} else {
 		headers = newHeader;
 	}
+	let headersLength = headers.length;
 	let rows = dataArray;
 	let pattern = '';
 	let regex;
-	let previoustextToSearch = '';
+	let previoustextToSearch;
 
 	function sortColumnOnClick(i) {
 		if (historyColumnsClick.includes(i)) {
 			const index = historyColumnsClick.indexOf(i);
 			historyColumnsClick.splice(index, 1);
-			setTimeout(function () {
+			/* setTimeout(function () { */
 				rows = rows.sort((a, b) => b[i].localeCompare(a[i]));
-			}, 10)
+			/* },5) */
 		} else {
-			setTimeout(function () {
+			/* setTimeout(function () { */
 				rows = rows.sort((a, b) => a[i].localeCompare(b[i]));
-			}, 10)
-			historyColumnsClick.push(i);
+				historyColumnsClick.push(i);
+			/* },5) */
 		}
-		sortColumns = true;
+		sortColumns=true;
 	}
 
-	function searchFunction(arr, target) {
-		return target.every((v) => arr.includes(v));
+	function searchFunction(string, itemsToSearch) {
+		return itemsToSearch.every((v) => string.includes(v));
 	}
 
 	/** Function that count occurrences of a substring in a string;
- * @param {String} string               The string
- * @param {String} subString            The sub string to search for
- * @param {Boolean} [allowOverlapping]  Optional. (Default:false)
- *
- * @author Vitim.us https://gist.github.com/victornpb/7736865
- * @see Unit Test https://jsfiddle.net/Victornpb/5axuh96u/
- * @see https://stackoverflow.com/a/7924240/938822
- */
-/* function occurrences(string, subString, allowOverlapping) {
+	 * @param {String} string               The string
+	 * @param {String} subString            The sub string to search for
+	 * @param {Boolean} [allowOverlapping]  Optional. (Default:false)
+	 *
+	 * @author Vitim.us https://gist.github.com/victornpb/7736865
+	 * @see Unit Test https://jsfiddle.net/Victornpb/5axuh96u/
+	 * @see https://stackoverflow.com/a/7924240/938822
+	 */
+	function occurrences(string, subString, allowOverlapping) {
 
-string += "";
-subString += "";
-if (subString.length <= 0) return (string.length + 1);
+		string += "";
+		subString += "";
+		if (subString.length <= 0) return (string.length + 1);
 
-var n = 0,
-	pos = 0,
-	step = allowOverlapping ? 1 : subString.length;
+		var n = 0,
+			pos = 0,
+			step = allowOverlapping ? 1 : subString.length;
 
-while (true) {
-	pos = string.indexOf(subString, pos);
-	if (pos >= 0) {
-		++n;
-		pos += step;
-	} else break;
-}
-return n;
-}
+		while (true) {
+			pos = string.indexOf(subString, pos);
+			if (pos >= 0) {
+				++n;
+				pos += step;
+			} else break;
+		}
+		return n;
+	}
 
-function occurencesMultiples(arr,string) {
-	return (arr.reduce(function (accumulator, currentValue) {
-		const occurencesNumber = occurrences(string,currentValue,false);
-		return accumulator + occurencesNumber;
-	}, 0));
-} */
+	function occurencesMultiples(arr, string) {
+		return (arr.reduce(function (accumulator, currentValue) {
+			const occurencesNumber = occurrences(string, currentValue, false);
+			return accumulator + occurencesNumber;
+		}, 0));
+	}
 
-/* .sort(function (a, b) {
-							occA = occurencesMultiples(search_items,a[4].toLowerCase());
-							occB = occurencesMultiples(search_items,b[4].toLowerCase());
-							return (occA - occB)}); */
+	let rowsFiltered = []
+	let rowsFilteredAndSorted = []
 
-	$: if (textToSearch !== '') {
-			pattern = '';
-			search_items = textToSearch.toLowerCase().split("+");
-			if (desactivateRegex === false) {
-				search_items.forEach((search_item) => {
-					pattern = pattern + "(?=.*" + search_item + ")";
-				});
-				regex = new RegExp(pattern, 'i');
-				try {
-					setTimeout(function () {
-						rows = dataArray.filter((row) => row.toString().toLowerCase().match(regex));
-					}, 5)
-				} catch (e) {
-					console.log("Invalid Regular Expression");
-					textToSearch == '';
-				}				
-			} else {
-					setTimeout(function () {
-						rows = dataArray.filter((row) => searchFunction(row.toString().toLowerCase(), search_items))
-						previoustextToSearch = textToSearch;
-					}, 5)						
-				}
-			} else {
-				rows = dataArray;
+	$: if (textToSearch !== '' && previoustextToSearch !== textToSearch) {
+		pattern = '';
+		search_items = textToSearch.toLowerCase().split("+");
+		if (desactivateRegex === false) {
+			search_items.forEach((search_item) => {
+				pattern = pattern + "(?=.*" + search_item + ")";
+			});
+			regex = new RegExp(pattern, 'i');
+			try {
+				/* setTimeout(function () { */
+					rows = dataArray.filter((row) => row.toString().toLowerCase().match(regex));
+					previoustextToSearch = textToSearch;
+				/* }, 5) */
+			} catch (e) {
+				console.log("Invalid Regular Expression");
+				textToSearch == '';
 			}
+		} else {
+			/* rows = dataArray.filter((row) => searchFunction(row.toString().toLowerCase(), search_items)); */
+			setTimeout(function () {
+			rows = dataArray;
+			for (const row of rows) {
+				const searchResults = searchFunction(row.toString().toLowerCase(), search_items)
+				if (searchResults) {
+					rowsFiltered = [...rowsFiltered, row]
+				}
+			}
+
+			for (const row of rowsFiltered) {
+				const score = occurencesMultiples(search_items, row.toString().toLowerCase());
+				const rowN = [...row, score];
+				rowsFilteredAndSorted = [...rowsFilteredAndSorted, rowN];
+			}
+			rowsFilteredAndSorted = rowsFilteredAndSorted.sort((a, b) => {
+				return b[headersLength] -
+					a[headersLength]
+			})
+			rows = rowsFilteredAndSorted.map(function (val) {
+				return val.slice(0, -1);
+			});
+			previoustextToSearch = textToSearch;
+			rowsFiltered = [];
+			rowsFilteredAndSorted = [];
+		},5)
+		}
+	} else {
+		if (textToSearch == '') {
+			rows = dataArray;
+		}
+	}
 </script>
 
-<table class:one-column="{headers.length===1}" class="{tableCSS}">
+<table class:one-column="{headersLength===1}" class="{tableCSS}">
 	{#if headers}	
 		<thead>
 			<tr>
@@ -139,10 +163,10 @@ function occurencesMultiples(arr,string) {
 	{/if}
 	<tbody bind:this={dataTable}>
 		{#if desactivateRegex==true && textToSearch==''}
-			<tr><td colspan="{headers.length}" class="info-search">Utilisez l'outil de recherche ci-dessus : les textes qui correspondent à la recherche s'afficheront ci-dessous</td></tr>
+			<tr><td colspan="{headersLength}" class="info-search">Utilisez l'outil de recherche ci-dessus : les textes qui correspondent à la recherche s'afficheront ci-dessous</td></tr>
 		{:else}
 			{#if previoustextToSearch != textToSearch && automaticSearch === false}
-				<tr><td colspan="{headers.length}"><p><span class="loader"></span></p><p class="info-search">Recherche en cours</p></td></tr>
+				<tr><td colspan="{headersLength}"><p><span class="loader"></span></p><p class="info-search">Recherche en cours</p></td></tr>
 			{:else}
 				{#if rows.length !=0}
 					{#each rows as row}
@@ -153,7 +177,7 @@ function occurencesMultiples(arr,string) {
 						</tr>
 					{/each}
 				{:else}
-					<tr><td colspan="{headers.length}" class="info-search">Aucun résultat trouvé</td></tr>
+					<tr><td colspan="{headersLength}" class="info-search">Aucun résultat trouvé</td></tr>
 				{/if}
 			{/if}
 		{/if}
