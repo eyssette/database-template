c562749425337925512cfd1bc9f84a36417ac8be
eyssette <cedric.eyssette@gmail.com>
2023-01-10 12:05:30 +0100

    :sparkles: Ajout possible de la préselection de conditions supplémentaires dans le hash

--- src/lib/AdditionalConditions.svelte
+++ src/lib/AdditionalConditions.svelte
@@ -3,10 +3,23 @@
 		additionalConditionsArray
 	} from './config.js';
 	export let textToSearch;
+	let conditionsHash;
+	let conditionsHashString;
+	let conditionsHashArray = [];
 	let conditionChecked = [];
 	$: additionalConditionsArray.forEach(
 		(condition, index) => {
-			if (conditionChecked[index] == true) {
+			conditionsHash=textToSearch.split('$');
+			textToSearch=conditionsHash[0];
+			conditionsHash.shift();
+			conditionsHashString=conditionsHash[0];
+			if(conditionsHashString) {
+				conditionsHashArray=conditionsHashString.split('C').map(v => parseInt(v));
+			} else {
+				conditionsHashArray=[]
+			}
+			if (conditionChecked[index] == true || conditionsHashArray.includes(index+1)) {
+				conditionChecked[index] = true
 				if (textToSearch!='2022\t') {textToSearch = textToSearch + condition[1];}
 			} else {
 				textToSearch = textToSearch.replace(condition[1], '');
--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -14,6 +14,7 @@
 	onMount(() => {
 		hash = window.location.hash;
 		if (hash) {
+			inputValue = hash.slice(1).split('$')[0];
 			textToSearch = hash.slice(1);
 		}
 	});
